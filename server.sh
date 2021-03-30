10b6d8ed95ea0cb57bce7f08f8d708eb14017914
Đỗ Đức Thái <46870069+Dec1mo@users.noreply.github.com>
2021-03-30 10:24:22 +0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# if [ -z "$PORT"]
-# then
-#   PORT=5005
-# fi
+if [ -z "$PORT"]
+then
+  $PORT=5005
+fi
 
 # rasa run --enable-api --port $PORT
-rasa run --endpoints ./app/endpoints.yml --credentials ./app/credentials.yml --enable-api --port 5005
+rasa run --endpoints ./app/endpoints.yml --credentials ./app/credentials.yml --enable-api --port $PORT
