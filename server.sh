8dd3e4cabee1f5574636e0484e5878feed92489f
Đỗ Đức Thái <46870069+Dec1mo@users.noreply.github.com>
2021-03-30 13:51:04 +0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -6,4 +6,4 @@ then
 fi
 
 # rasa run --enable-api --port $PORT
-rasa run --endpoints ./app/endpoints.yml --credentials ./app/credentials.yml --enable-api --port $PORT
+rasa run --endpoints /app/endpoints.yml --credentials /app/credentials.yml --enable-api --port $PORT
